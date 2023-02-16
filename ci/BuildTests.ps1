4743160ee4fae506bb7737874cb85931857ff636
poipoiyo <48159287+poipoiyo@users.noreply.github.com>
2023-02-16 20:40:19 +0800

    Add: yml file and scripts

---
+++ ci/BuildTests.ps1
@@ -0,0 +1,21 @@
+param(
+    [String] $Configuration,
+	[String] $Platform
+)
+
+$Plat_Param = ""
+[String]$Root = Get-Location
+
+if ($Platform -eq "x64") {
+  $Plat_Param = "amd64"
+}
+elseif ($Platform -eq "Win32") {
+  $Plat_Param = "x86"
+}
+
+cmd.exe -/c call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" $Plat_Param 
+msbuild $Root/Actions.sln -maxcpucount:4 /t:DriverTest:rebuild /p:configuration=$Configuration /p:platform=$Platform
+
+exit $LASTEXITCODE
+
+
